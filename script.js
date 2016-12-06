aa29a5bdde37ef9e482a8e6a44934f1cf0703df7
runnedrunner <auszhou@nuevaschool.org>
2016-12-06 08:54:39 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -8,24 +8,24 @@ $.ajax({
   }
 })
 
-var cloud = d3.layout.cloud;
-
 var fill = d3.scale.category20();
 
-var layout = cloud()
-.size([500, 500])
-.words([
-  "Hello", "world", "normally", "you", "want", "more", "words",
-  "than", "this"].map(function(d) {
-  return {text: d, size: 10 + Math.random() * 90, test: "haha"};
-}))
-.padding(5)
-.rotate(function() { return ~~(Math.random() * 1) * 90; })
-.font("Impact")
-.fontSize(function(d) { return d.size; })
-.on("end", draw);
+makeCloud(["hi", "there", "friedn"])
 
-layout.start();
+function makeCloud(words) {
+  var layout = d3.layout.cloud()
+  .size([500, 500])
+  .words(words.map(function(d) {
+    return {text: d, size: 10 + Math.random() * 90, test: "haha"};
+  }))
+  .padding(5)
+  .rotate(function() { return ~~(Math.random() * 2) * 90; })
+  .font("Impact")
+  .fontSize(function(d) { return d.size; })
+  .on("end", draw);
+
+  layout.start(); 
+}
 
 function draw(words) {
   d3.select("body").append("svg")
@@ -38,7 +38,7 @@ function draw(words) {
     .enter().append("text")
     .style("font-size", function(d) { return d.size + "px"; })
     .style("font-family", "Impact")
-    .style("fill", function(d, i) { return '#000'; })
+    .style("fill", function(d, i) { return fill(i); })
     .attr("text-anchor", "middle")
     .attr("transform", function(d) {
     return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
